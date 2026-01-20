8f08f839cd7fec3d5d8eae3445e1fe7b9ad274ae
GreenreaperX <>
2026-01-20 10:34:24 +0000

    While loops

---
+++ the_while_loop/while_loops/main.py
@@ -0,0 +1,14 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+i = 0
+# List to hold the selected countries
+selected = []
+while i  <= 20:
+        country = countries[i] 
+        selected.append(country)
+        i+=1
+        
+
+
+# Testing
+print('Selected countries:', selected)
